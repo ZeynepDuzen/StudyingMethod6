c918d2bd650d6769bb3df28b04696143a2830d7b
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 06:52:33 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod6
+{
+    internal class Program
+    {
+        static void Identitiy(string name, int age )
+        {
+            Console.WriteLine(name + " " + "Flower" + " " + "is" + " " + age + " " + "years old");
+        }
+        static void Main(string[] args)
+        {
+            Identitiy("Sam",22);
+            Identitiy("Lucy",33);
+            Identitiy("Carol",38);
+            Identitiy("Jimy",36);
+
+            Console.ReadLine();
+
+        }
+    }
+}
